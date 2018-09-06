5bc7cc150ee7aabdfe37811b22edcd091c903cbc
Unknown <regpo55555@gmail.com>
2018-09-06 15:39:42 +0600

    changed concat to push in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,20 +2,34 @@ import Check from "../containers/Checks/Check/Check";
 
 const initialState = {
     checks: [],
-    checkId: 0
+    checkId: 0,
+    initChecks: []
 }
 const reducer = (state = initialState, action) => {
     // console.log(state.checks[state.checkId - 1])
     switch (action.type) {
         case 'ADD_CHECK':
+        let newChecks = state.checks;
+        // newChecks = newChecks.push({ date: new Date(), id: state.checkId, members: action.check });
+        // newChecks.splice(2, 1)
+        newChecks.push({ date: new Date(), id: state.checkId, members: action.check });
+        
+
+        // newChecks[state.checkId].members=null;
+        
+        // // newChecks.push('adf')
+        console.log(newChecks);
+        console.log(state.checks)
+        // newArray.
             return {
                 ...state,
-                checks: state.checks.concat({ date: new Date(), id: state.checkId, members: action.check }),
+                // checks: state.checks.concat({ date: new Date(), id: state.checkId, members: action.check }),
+                checks: newChecks,
                 checkId: state.checkId + 1,
             }
 
         case 'ADD_MEMBER':
-            console.log(state.checks[0].members)
+            console.log(state.checkId-1)
             const newArray = state.checks;
             newArray[state.checkId-1].members.push({ dish: action.member })
             return {
